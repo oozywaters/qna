9dafcd8365e805349a1b8ef55a917b44678379d6
oozywaters <oozywaters@Mac-mini-111.local>
2018-05-23 23:23:32 +0300

    Add questions controller new/create actions

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -9,7 +9,25 @@ class QuestionsController < ApplicationController
 
   end
 
+  def new
+    @question = Question.new
+  end
+
+  def create
+    @question = Question.create(question_params)
+
+    if @question.save
+      redirect_to @question
+    else
+      render :new
+    end
+  end
+
   private
+
+  def question_params
+    params.require(:question).permit(:title, :body)
+  end
 
   def find_question
     @question = Question.find(params[:id])

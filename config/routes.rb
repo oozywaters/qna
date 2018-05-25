916b084ce26e5b2f234c6b9192be875d296b3c02
oozywaters <zavertannyi@banki.ru>
2018-05-25 17:42:44 +0300

    Add create question feature test

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "home#index"
+  root to: "questions#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :questions, shallow: true do
     resources :answers

e5eee3e9c266d60370c2fefd58f5d0c7aa5e512f
oozywaters <oozy.waters@gmail.com>
2018-05-26 14:49:05 +0300

    Add model associations tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should have_many(:questions) }
+  it { should have_many(:answers) }
+
+  describe "#author_of?(resource)" do
+    let(:user) { create(:user) }
+    let(:another_user) { create(:user) }
+    let(:question) { create(:question, user: user) }
+
+    it "user is the author" do
+      expect(user).to be_author_of(question)
+    end
+    it "user is not an author" do
+      expect(another_user).not_to be_author_of(question)
+    end
+  end
 end

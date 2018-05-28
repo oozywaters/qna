f849e26cdfbb507e8135619762706ecd27769262
oozywaters <zavertannyi@banki.ru>
2018-05-28 18:03:26 +0300

    Fix post#create of answers controller

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -3,7 +3,6 @@ class AnswersController < ApplicationController
   before_action :find_question, only: %i[create]
 
   def create
-    @answers = @question.answers
     @answer = @question.answers.build(answer_params)
     @answer.user = current_user
     @answer.save

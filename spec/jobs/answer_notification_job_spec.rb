2d9a373d7c26e0b82b4240f1cb4ed84207aec6bf
oozywaters <oozy.waters@gmail.com>
2018-06-25 14:18:29 +0300

    Refactor specs

--- spec/jobs/answer_notification_job_spec.rb
+++ spec/jobs/answer_notification_job_spec.rb
@@ -6,15 +6,18 @@ RSpec.describe AnswerNotificationJob, type: :job do
   let!(:answer) {create(:answer, question: question, user: user)}
   let!(:users) { create_list(:user, 5) }
 
-  it 'sends answer' do
-    question.unsubscribe(user)
-    expect(AnswerNotificationMailer).to_not receive(:notify).with(user, answer).and_call_original
+  before { question.unsubscribe(user) }
 
+  it 'should send notifications to subscribed users' do
     users.each do |user|
       question.subscribe(user)
       expect(AnswerNotificationMailer).to receive(:notify).with(user, answer).and_call_original
     end
 
     AnswerNotificationJob.perform_now(answer)
+  end
+
+  it 'should not send notifications to unsubscribed users' do
+    expect(AnswerNotificationMailer).to_not receive(:notify).with(user, answer).and_call_original
   end
 end

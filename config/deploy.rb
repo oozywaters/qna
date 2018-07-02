0d05672df12743748c1b3a5bd1e48779ef5e867b
oozywaters <oozy.waters@gmail.com>
2018-07-02 23:48:28 +0300

    Remove thinking sphinx config

--- config/deploy.rb
+++ config/deploy.rb
@@ -22,7 +22,7 @@ set :deploy_user, "deployer"
 # set :pty, true
 
 # Default value for :linked_files is []
-append :linked_files, "config/database.yml", ".env"
+append :linked_files, "config/database.yml", "config/thinking_sphinx.yml", ".env"
 
 # Default value for linked_dirs is []
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system", "public/uploads"

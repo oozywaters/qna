b0b57fc67779b6ed5e4c52489693ed292caa03eb
oozywaters <oozy.waters@gmail.com>
2018-08-04 19:08:19 +0300

    Add QuestionSummary component

---
+++ frontend/components/QuestionSummary/QuestionSummary.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class QuestionSummary extends Component {
+    render() {
+        return (
+            <div>Question Summary</div>
+        );
+    }
+}
+
+export default QuestionSummary;
